386d717b9e68bc90098c2853fed265014e02d101
Hellopackets89 <Brandon@allmark.me>
2024-07-11 23:27:44 +0800

    commenting my code

--- function_app.py
+++ function_app.py
@@ -20,27 +20,29 @@ def timer_trigger1(myTimer: func.TimerRequest, context: func.Context) -> None:
         if myTimer.past_due:
             logging.info('The timer is past due!')
 
-        # Get the connection string from the environmental settings
-        #This connects to my blob storage and my SQL DB
+        # Get the connection strings from the environmental settings
+        # This connects to my blob storage and my SQL DB
         conn_str = os.getenv('SQLDB_CONNECTION_STRING')
         blob_service_client = BlobServiceClient.from_connection_string(os.getenv('AzureWebJobsStorage'))
         logging.info('Attempting connection')
         # Create a new connection
         conn = pyodbc.connect(conn_str)
 
-        # Create a cursor from the connection
+        # Create a cursor from the connection. The cursor is what interacts with my database. 
+        # The cursor can be used to "move around" the database and fetch individual rows. I'm just printing everything. 
         cur = conn.cursor()
+
+        # Query my database and get data from the ResumeVisitors table
         logging.info('Attempting query..')
-        # Execute a query
         cur.execute("SELECT TOP (1000) * FROM ResumeVisitors")
 
-        # Create a blob client using the local file name as the name for the blob
-        #putting "f" infront of strings means it parses the variables inside the string, instead of just putting the name of the variable. 
+        # Set the filename I plan to use for my results
+        # putting "f" infront of strings means it parses the variables inside the string, instead of just putting the name of the variable. 
         today = datetime.date.today().strftime("%Y%m%d")
         filename = f"visitors{today}.txt"
         blob_client = blob_service_client.get_blob_client("results", filename)
 
-        #Fetch all the results of the query and add it to my file
+        # Fetch all the results of the query and add it to my file
         rows = cur.fetchall()
         all_rows = []
         #For each row found in the query results, combine them into one string. 
@@ -49,13 +51,15 @@ def timer_trigger1(myTimer: func.TimerRequest, context: func.Context) -> None:
 
         all_rows_str = '\n'.join(all_rows)
         logging.info(all_rows_str)
-        # Upload the row to the blob
+        # Upload all the results to the storage account using the predefined filename. 
         blob_client.upload_blob(all_rows_str)
 
-    #Error logging - this section provides more verbose errors if the function app fails for whatever reason.
+    # Error logging - this section provides more verbose errors if the function app fails for whatever reason.\
+    # \n refers to printing a new line
+    # this error logging was ripped from microsoft learn
     except pyodbc.Error as ex:
         sqlstate = ex.args[0] if len(ex.args) > 0 else None
-        logging.error(f'Database error occurred:\\nSQLState: {sqlstate}\\nError: {ex}')
+        logging.error(f'Database error occurred:\nSQLState: {sqlstate}\nError: {ex}')
         if context.retry_context.retry_count == context.retry_context.max_retry_count:
             logging.info(
                 f"Max retries of {context.retry_context.max_retry_count} for "
