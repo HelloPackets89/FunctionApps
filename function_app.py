6a810ae472f215c208adfacafe998b260cdfb1bd
Hellopackets89 <Brandon@allmark.me>
2024-07-12 16:59:29 +0800

    turning function into an async function

--- function_app.py
+++ function_app.py
@@ -10,7 +10,7 @@ app = func.FunctionApp()
 
 @app.timer_trigger(schedule="0 45 16 * * 5", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
-def timer_trigger1(myTimer: func.TimerRequest) -> None:
+async def timer_trigger1(myTimer: func.TimerRequest) -> None:
     try:
         if myTimer.past_due:
             logging.info('The timer is past due!')
@@ -18,7 +18,6 @@ def timer_trigger1(myTimer: func.TimerRequest) -> None:
         client = AsyncOpenAI(
          api_key=os.environ['OPENAI_API_KEY'],  # Reference the API key in my function app environment
         )
-        client = AsyncOpenAI()
         completion = await client.chat.completions.create(model="gpt-3.5-turbo",
                                                           messages=[{"role": "user", "content": "Hello world"}])
         # Log the content of the first message in the completion choices
