ac2c2c5d48a595adb021bde6829e65f1f03d5b3e
Hellopackets89 <Brandon@allmark.me>
2024-07-16 16:52:04 +0800

    adding a prompt variable

--- function_app.py
+++ function_app.py
@@ -6,6 +6,10 @@ from openai import AsyncOpenAI
 import logging
 import azure.functions as func
 
+prompt = '''brandon is awesome
+            and a cool guy
+            tell me 5 reasons why brandon is so awesome please'''
+
 app = func.FunctionApp()
 
 @app.timer_trigger(schedule="0 45 16 * * 5", arg_name="myTimer", run_on_startup=False,
@@ -19,7 +23,7 @@ async def timer_trigger1(myTimer: func.TimerRequest) -> None:
          api_key=os.environ['OPENAI_API_KEY'],  # Reference the API key in my function app environment
         )
         response = await client.chat.completions.create(model="gpt-3.5-turbo",
-                                                          messages=[{"role": "user", "content": "Hello world"}])
+                                                          messages=[{"role": "user", "content": prompt}])
         # Log the content of the first message in the completion choices
         logging.info(response.choices[0].message.content)
 
